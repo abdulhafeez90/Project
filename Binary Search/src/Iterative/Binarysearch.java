4d25f0ee9a0a1cffa41ae667a256717ad1d65156
Abdul Hafeez <sabdulhafeez01@gmail.com>
2020-03-24 21:32:58 +0530

    Final commit

---
+++ Binary Search/src/Iterative/Binarysearch.java
@@ -0,0 +1,28 @@
+package Iterative;
+
+class Binarysearch {
+	public int binarySearch(int arr[], int x) {
+		int l = 0;
+		int r = arr.length - 1;
+		while(l <= r) {
+			int mid = l + (r - l) / 2;
+			if(arr[mid] == x)
+				return mid;
+			if(arr[mid] < x)
+				l = mid + 1;
+			else
+				r = mid - 1;
+		}
+		return -1;
+	}
+	public static void main (String[] args) {
+		Binarysearch b = new Binarysearch();
+		int arr[] = {10,20,30,40,50,60,70,80,90,100};
+		int x = 100;
+		int result = b.binarySearch(arr, x);
+		if(result == -1)
+			System.out.println("Element not found");
+		else
+			System.out.println("Element " +x+ " is at index " +result+ ".");
+	}
+}
--- Binary Search/src/Recursive/Binarysearch.java
+++ Binary Search/src/Recursive/Binarysearch.java
@@ -1,21 +1,25 @@
+package Recursive;
+
 class Binarysearch {
 	public int binarySearch(int arr[], int l, int r, int x) {
-		if(r>=1) {
-			int mid = l+(r-1)/2;
+		if(r >= l) {
+			int mid = l + (r - l) / 2;
 			if(arr[mid] == x)
 				return mid;
-			if(arr[mid]>x)
-				return binarySearch(arr,l,mid-1,x);
-			return binarySearch(arr,mid+1,r,x);
+			if(arr[mid] > x)
+				return binarySearch(arr, l, mid-1, x);
+			else
+				return binarySearch(arr, mid+1, r, x);
 		}
 		return -1;
 	}
 	public static void main(String[] args) {
 		Binarysearch b = new Binarysearch();
-		int arr[] = {2,3,4,10,40};
-		int n = arr.length;
-		int x = 3;
-		int result = b.binarySearch(arr, 0, n-1, x);
+		int arr[] = {2,4,6,8,10};
+		int l = 0;
+		int n = arr.length - 1;
+		int x = 10;
+		int result = b.binarySearch(arr, l, n, x);
 		if(result == -1)
 			System.out.println("Element not found");
 		else
